ba4180f05a1adbf8e359717d0499781f4a400be8
David Lei <DavidtheDefender2003@gmail.com>
2023-01-27 15:54:15 -0800

    Modify comments for variables

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ const resetButton = document.querySelector(".reset-btn") // Reset button
 const lapsSection = document.querySelector(".laps-section"); // Laps section to display all lapped times
 
 let intervalId = null; // Interval ID used to change the timer display
-let startTime = 0; // Time when start button is clicked
-let currentTime = 0; // Current time displayed on time display
-let stopTime = 0; // Time when stop button is clicked
+let startTime = 0; // Time when start button is clicked (in milliseconds)
+let currentTime = 0; // Current time displayed on time display (in milliseconds)
+let stopTime = 0; // Time when stop button is clicked (in milliseconds)

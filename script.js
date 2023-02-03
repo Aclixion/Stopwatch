28aba90f60e1bdcfa9ecee906690369c149546b5
David Lei <DavidtheDefender2003@gmail.com>
2023-02-02 16:11:21 -0800

    Add clear button variable

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const startButton = document.querySelector(".start-btn"); // Start timer button
 const stopButton = document.querySelector(".stop-btn"); // Stop timer button
 const lapButton = document.querySelector(".lap-btn"); // Lap button
 const resetButton = document.querySelector(".reset-btn") // Reset button
-
+const clearButton = document.querySelector(".clear-btn"); // Clear button
 const lapsSection = document.querySelector(".laps-section"); // Laps section to display all lapped times
 
 let intervalId = null; // Interval ID used to change the timer display
